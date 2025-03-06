f6a0a5719f19dcc96411447cbd4c0a3d7e8ec3a1
Aleksandr-Zhazhoyan <zhazhoyan85@gmail.com>
2025-03-06 22:49:41 +0400

    Initial Commit

---
+++ HomeWork-16-01-25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-16-01-25
+//
+//  Created by Aleksandr Zhazhoyan on 06.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
